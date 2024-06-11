22a95e8300382619ac8d646cc316060f54b9ddfd
keitumetse mokwena <106819479+Keitu947@users.noreply.github.com>
2024-06-11 14:25:39 +0200

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,30 @@
+let name = "KEITUMETSE MOKWENA"; 
+
+let age = 29; 
+
+let isStudent = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+
+let favoriteColour = "Pink";
+
+console.log("Favorite Colour:", favoriteColour);
+
+favoriteColour = "Yellow";
+
+console.log("New Favorite Colour:", favoriteColour);
+
+let num1 = 7;
+let num2 = 17;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
