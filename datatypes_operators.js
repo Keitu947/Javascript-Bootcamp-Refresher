fbd32f613006864ce31c6981616c04a33a8d9431
keitumetse mokwena <106819479+Keitu947@users.noreply.github.com>
2024-06-11 14:36:40 +0200

    Create datatypes_operators.js

---
+++ datatypes_operators.js
@@ -0,0 +1,55 @@
+let number1 = 17;
+
+let number2 = 7.10;
+
+let add = number1 + number2;
+let subtract = number1 - number2;
+let multiply = number1 * number2;
+let divide = number1 / number2;
+let modulus = number1 % number2;
+let exponentiation = number1 ** number2;
+
+console.log("Add:", add);
+console.log("Subtract:", subtract);
+console.log("Multiply:", multiply);
+console.log("Divide:", divide);
+console.log("Modulus:", modulus);
+console.log("Exponentiation:", exponentiation);
+
+let comparison1 = num1 > num2;
+let comparison2 = num1 <= num2;
+let comparison3 = num1 === num2;
+let comparison4 = num1 !== num2;
+
+console.log("Comparison1 (num1 > num2):", comparison1);
+console.log("Comparison2 (num1 <= num2):", comparison2);
+console.log("Comparison3 (num1 === num2):", comparison3);
+console.log("Comparison4 (num1 !== num2):", comparison4);
+
+let x = 8;
+let y = 12;
+
+console.log("x is greater than y:", x > y);
+console.log("x is less than or equal to y:", x <= y);
+console.log("x is equal to y:", x === y);
+console.log("x is not equal to y:", x !== y);
+
+let a = true;
+let b = false;
+
+console.log("a AND b:", a && b);
+console.log("a OR b:", a || b);
+console.log("NOT a:", !a);
+
+let p = 10;
+
+p += 5;
+console.log("p after += 5:", p);
+p -= 3;
+console.log("p after -= 3:", p);
+p *= 2;
+console.log("p after *= 2:", p);
+p /= 4;
+console.log("p after /= 4:", p);
+p %= 3;
+console.log("p after %= 3:", p);
